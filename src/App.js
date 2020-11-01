a8d86676690c8901a4be721e6c2365539686cbe6
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-11-01 15:22:28 +0100

    [HEADER] Add countries to dropdown from api dynamically

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import { 
-  FormControl,
-  MenuItem, 
-  Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
-// import logo from '../public/coronavirus-img.png';
+import {
+  FormControl,
+  MenuItem,
+  Select
+} from '@material-ui/core';
+
+
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  // useEffect
+  useEffect(() => {
+    // create an async function, make a req to the server, wait for response, use the resp data
+
+      const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }));
+
+            setCountries(countries);
+        })
+      };
+      getCountriesData();
+  }, []);
+
+  // when select dropdown option is selected by User
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    console.log('[You selected]: ', countryCode);
+    setCountry(countryCode);
+  };
+
   return (
     <div className="App">
-      <img src="coronavirus-img.png" alt="logo"/>
-     <h1>Covid-19-Tracker</h1>
-     <FormControl className="app__dropdown">
-        <Select
-          variant="outlined"
-          value="abc"
-        >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
 
-        </Select>
-     </FormControl>
+      <div className="app__header">
+        <img src="coronavirus-img.png" alt="logo" className="app__logo" />
+
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value={country}
+            onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+            {
+              countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+
+          </Select>
+        </FormControl>
+      </div>
+
     </div>
   );
 }
