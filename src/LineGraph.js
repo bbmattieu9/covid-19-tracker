17df2547131c9fd606ed6d289212d16029ab5221
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-11-10 12:31:09 +0100

    [LineGraph] Add Graph options

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -4,97 +4,92 @@ import numeral from "numeral";
 
 
 const options = {
-    legend: {
-        display: false,
-    },
-    elements: {
-        point: {
-            radius: 0
-        },
-    },
-    maintainAspectRatio: false,
-    tooltips: {
-        mode: "index",
-        intersect: false,
-        callbacks: {
-            label: function (tooltipItem, data) {
+     legend: {
+         display: false
+     },
+     elements: {
+         point: {
+             radius: 0
+         }
+     },
+     maintainAspectRatio:false,
+     tooltips: {
+         mode: "index",
+         intersect: false,
+         callbacks: {
+            llabel: function(tooltipItem, data) {
                 return numeral(tooltipItem.value).format("+0,0");
-            },
-        },
-    },
-    scales: {
-        xAxes: [{
-            type: "times",
-            time: {
-                format: 'MM/DD/YY',
-                tooltipFormat: 'll',
-            },
-        },
-    ],
-    yAxes: [{
-        gridLines: {
-            display: false,
-        },
-        ticks: {
-            callback: function (value, index, values) {
-                return numeral(value).format("0a");
-            } ,
-        },
-    },],
-    },
+            }
+         }
+     },
+     scales: {
+         xAxes: [{
+             type: "time",
+             time: {
+                 format: "MM/DD/YY",
+                 tooltipFormat: 'll'
+             }
+         }],
+         yAxes: [{
+             gridLines: {
+                 display: false
+             },
+             ticks: {
+                 callback: function(value, index, values) {
+                     return numeral(value).format("0a");
+                 }
+             }
+         }]
+     }
+       
 };
 
 function LineGraph({ casesType = "cases"}) {
 
     const [data, setData] = useState({});
 
-    const buildChartData = (data, casesType = "cases") => {
+    const buildChartData = (data, casesType = 'cases') => {
         const chartData = [];
         let lastDataPoint;
-         for (let date in data.cases) {
+         for (let date in data.cases) { 
             if(lastDataPoint) {
-                const newDataPoint = {
+                const newDataPoint = { 
                     x: date,
                     y: data[casesType][date] - lastDataPoint
-                };
+                }; 
                 chartData.push(newDataPoint);
             }
-            lastDataPoint = data[casesType][data];
+            lastDataPoint = data[casesType][date];
         }
         return chartData;
-    }
+    };
+    
 
     useEffect(() => {
         const fetchDataFromAPI = async () => {
           await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
         .then((response) => response.json())
         .then((data) => {
-            console.log('[Data from server]', data);
             const chartData = buildChartData(data);
             setData(chartData);
         });
         }
         fetchDataFromAPI();
-    }, [casesType]);
-
-    
+    }, []);
 
     return (
         <div>
-
-            <h1>I am a Line Graph</h1>
             {data?.length > 0 && (
                  <Line
-                 options= {options}
-                 data={{
-                  datasets: [{
+                 options ={options}
+                    data={{
+                     datasets: [{
                       backgroundColor: "rgba(204, 16, 52, 0.6)",
                       borderColor: "#CC1034",
                       data: data
                   }]
               }} />
-            )}
-           
+            )} 
         </div>
     )
 }
